d402d2eea12d28aeed731a01af9beccc58790bed
amdymstfa <amdymoustapha011@gmail.com>
2025-10-24 17:21:24 +0100

    Iniatialize project with spring

---
+++ src/main/java/com/yallatawsil/backend/YallatawsilBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yallatawsil.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YallatawsilBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YallatawsilBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yallatawsil/backend/YallatawsilBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yallatawsil.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YallatawsilBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
