ddeb1801f4ba28969085d1cf70d0a6b112d082c6
amdymstfa <amdymoustapha011@gmail.com>
2025-10-25 17:55:07 +0100

    feat(enum): implement VehicleType enum with capacity attributes and Lombok getters

---
+++ src/main/java/com/yallatawsil/backend/entity/enums/VehicleType.java
@@ -0,0 +1,22 @@
+package com.yallatawsil.backend.entity.enums ;
+
+
+import lombok.Getter;
+
+@Getter
+public enum VehicleType {
+//  format of enum : enumName(defaultMaxWeight,defaultMaxVolume,defaultMaxDeliveries)
+    BIKE(50.0, 0.5, 15),
+    VAN(1000.0, 8.0, 50),
+    TRUCK(5000.0, 40.0, 100);
+
+    private final Double defaultMaxWeight ;
+    private final Double defaultMaxVolume ;
+    private final int defaultMaxDeliveries ;
+
+    VehicleType(Double defaultMaxWeight, Double defaultMaxVolume, int defaultMaxDeliveries){
+        this.defaultMaxWeight = defaultMaxWeight ;
+        this.defaultMaxVolume = defaultMaxVolume ;
+        this.defaultMaxDeliveries = defaultMaxDeliveries ;
+    }
+}
