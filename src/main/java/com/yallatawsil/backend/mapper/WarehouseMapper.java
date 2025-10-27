ef6b61d9bdae0daa8aa5c61e588dedc05d265641
amdymstfa <amdymoustapha011@gmail.com>
2025-10-27 16:09:20 +0100

    feat: add WarehouseMapper for entity-DTO conversions

---
+++ src/main/java/com/yallatawsil/backend/mapper/WarehouseMapper.java
@@ -0,0 +1,14 @@
+package com.yallatawsil.backend.mapper ;
+
+import com.yallatawsil.backend.dto.request.WarehouseRequestDTO;
+import com.yallatawsil.backend.dto.response.WarehouseResponseDTO;
+import com.yallatawsil.backend.entity.Warehouse;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface WarehouseMapper {
+    // Convert dto to entity
+    WarehouseResponseDTO toResponseDTO(Warehouse warehouse);
+    // Convert entity to dto
+    Warehouse toEntity(WarehouseRequestDTO dto);
+}
