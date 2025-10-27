9e15244ceb6b2a666c8186a5cb6ccfc4f5bf17fa
amdymstfa <amdymoustapha011@gmail.com>
2025-10-27 10:13:09 +0100

    feat(dto): add validation rules for VehicleRequestDTO with positive value constraints
    - Applied @Positive instead of @PositiveOrZero for numeric fields
    - Ensured maxWeight, maxDeliveries, and maxVolume must be strictly greater than 0
    - Kept null values valid to allow default initialization in service layer
    - Improved validation messages for clarity and consistency

---
+++ src/main/java/com/yallatawsil/backend/dto/request/VehicleRequestDTO.java
@@ -0,0 +1,36 @@
+package com.yallatawsil.backend.dto.request ;
+
+/**
+ * DTO for creating or updating a Vehicle entity.
+ * Contains validation rules for input data.
+ */
+
+
+import com.yallatawsil.backend.entity.enums.VehicleType;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class VehicleRequestDTO {
+
+    @NotNull(message = "Type of vehicle required")
+    private VehicleType type ;
+
+    @NotNull(message = "License plate is required")
+    private String licensePlate ;
+
+    @Positive(message = "Weight must be greater than 0")
+    private Double maxWeight ;
+
+    @Positive(message = "Deliveries must be greater than 0")
+    private Double maxDeliveries ;
+
+    @Positive(message = "Volume must be greater than 0")
+    private  Double maxVolume ;
+}
