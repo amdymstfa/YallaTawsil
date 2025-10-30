b0dfab29c083f53e151c5e0ae6119ed456e5bc89
amdymstfa <amdymoustapha011@gmail.com>
2025-10-30 16:12:17 +0100

    fix(service): align VehicleServiceImpl with BaseServiceImpl generics and correct toEntity return type

---
+++ src/main/java/com/yallatawsil/backend/service/InterfaceEntityImpl/VehicleServiceImpl.java
@@ -0,0 +1,60 @@
+package com.yallatawsil.backend.service.InterfaceEntityImpl ;
+
+import com.yallatawsil.backend.dto.request.VehicleRequestDTO;
+import com.yallatawsil.backend.dto.response.VehicleResponseDTO;
+import com.yallatawsil.backend.entity.Vehicle;
+import com.yallatawsil.backend.mapper.VehicleMapper;
+import com.yallatawsil.backend.repository.VehicleRepository;
+import com.yallatawsil.backend.service.BaseServiceImpl.BaseServiceImpl;
+import com.yallatawsil.backend.service.InterfaceEntity.VehicleService;
+import jakarta.persistence.Entity;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Slf4j
+
+public class VehicleServiceImpl extends BaseServiceImpl<Vehicle, VehicleRequestDTO, VehicleResponseDTO, Long>
+        implements VehicleService {
+
+    private final VehicleRepository vehicleRepository;
+    private final VehicleMapper vehicleMapper;
+
+    public VehicleServiceImpl(VehicleRepository vehicleRepository, VehicleMapper vehicleMapper){
+        super(vehicleRepository, "Vehicle");
+        this.vehicleRepository = vehicleRepository ;
+        this.vehicleMapper = vehicleMapper ;
+    }
+
+    @Override
+    protected Vehicle toEntity(VehicleRequestDTO dto) {
+        return null;
+    }
+
+    @Override
+    protected VehicleResponseDTO toResponseDTO(Entity entity) {
+        return null;
+    }
+
+    @Override
+    protected void updateEntityFromDTO(VehicleRequestDTO dto, Vehicle entity) {
+
+    }
+
+    @Override
+    protected VehicleResponseDTO toResponseDTO(Vehicle vehicle) {
+        return null;
+    }
+
+    @Override
+    protected Long getEntityId(Vehicle vehicle) {
+        return 0L;
+    }
+
+    @Override
+    public List<VehicleResponseDTO> findAvailableVehicles(LocalDate date) {
+        return List.of();
+    }
+}
+
