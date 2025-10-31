61ed9db17c478fd879a4279278a45d561021f11e
amdymstfa <amdymoustapha011@gmail.com>
2025-10-31 11:30:29 +0100

    Add WarehouseService interface

---
+++ src/main/java/com/yallatawsil/backend/service/InterfaceEntity/WarehouseService.java
@@ -0,0 +1,9 @@
+package com.yallatawsil.backend.service.InterfaceEntity ;
+
+import com.yallatawsil.backend.dto.request.WarehouseRequestDTO;
+import com.yallatawsil.backend.dto.response.WarehouseResponseDTO;
+import com.yallatawsil.backend.service.BaseService.BaseService;
+
+public interface WarehouseService extends BaseService<WarehouseRequestDTO, WarehouseResponseDTO, Long> {
+
+}
