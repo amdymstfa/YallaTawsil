c0c45787e049aa532e734eebb8bcdcd9c18c6a50
amdymstfa <amdymoustapha011@gmail.com>
2025-11-03 00:31:01 +0100

    Fix VehicleServiceImpl: implement toEntity, toResponseDTO, updateEntityFromDTO, getEntityId

--- src/main/java/com/yallatawsil/backend/service/InterfaceEntityImpl/VehicleServiceImpl.java
+++ src/main/java/com/yallatawsil/backend/service/InterfaceEntityImpl/VehicleServiceImpl.java
@@ -1,4 +1,4 @@
-package com.yallatawsil.backend.service.InterfaceEntityImpl ;
+package com.yallatawsil.backend.service.InterfaceEntityImpl;
 
 import com.yallatawsil.backend.dto.request.VehicleRequestDTO;
 import com.yallatawsil.backend.dto.response.VehicleResponseDTO;
@@ -8,56 +8,59 @@ import com.yallatawsil.backend.repository.VehicleRepository;
 import com.yallatawsil.backend.service.BaseServiceImpl.BaseServiceImpl;
 import com.yallatawsil.backend.service.InterfaceEntity.VehicleService;
 import jakarta.persistence.Entity;
-import lombok.Getter;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Slf4j
-@Setter
-@Getter
 public class VehicleServiceImpl extends BaseServiceImpl<Vehicle, VehicleRequestDTO, VehicleResponseDTO, Long>
         implements VehicleService {
 
     private final VehicleRepository vehicleRepository;
     private final VehicleMapper vehicleMapper;
 
-    public VehicleServiceImpl(VehicleRepository vehicleRepository, VehicleMapper vehicleMapper){
+    public VehicleServiceImpl(VehicleRepository vehicleRepository, VehicleMapper vehicleMapper) {
         super(vehicleRepository, "Vehicle");
-        this.vehicleRepository = vehicleRepository ;
-        this.vehicleMapper = vehicleMapper ;
+        this.vehicleRepository = vehicleRepository;
+        this.vehicleMapper = vehicleMapper;
     }
 
     @Override
     protected Vehicle toEntity(VehicleRequestDTO dto) {
-        return null;
-    }
-
-    @Override
-    public VehicleResponseDTO toResponseDTO(Entity entity) {
-        return null;
-    }
-
-    @Override
-    protected void updateEntityFromDTO(VehicleRequestDTO dto, Vehicle entity) {
-
+        return vehicleMapper.toEntity(dto);
     }
 
     @Override
     protected VehicleResponseDTO toResponseDTO(Vehicle vehicle) {
-        return null;
+        return vehicleMapper.toResponseDTO(vehicle);
+    }
+
+
+    @Override
+    protected void updateEntityFromDTO(VehicleRequestDTO dto, Vehicle vehicle) {
+        vehicle.setType(dto.getType());
+        vehicle.setMaxWeight(dto.getMaxWeight());
+        vehicle.setMaxVolume(dto.getMaxVolume());
+        vehicle.setMaxDeliveries(dto.getMaxDeliveries());
+        vehicle.setLicensePlate(dto.getLicensePlate());
     }
 
     @Override
     protected Long getEntityId(Vehicle vehicle) {
-        return 0L;
+        return vehicle.getId();
+    }
+
+    @Override
+    public VehicleResponseDTO toResponseDTO(Entity entity) {
+        return vehicleMapper.toResponseDTO((Vehicle) entity);
     }
 
     @Override
     public List<VehicleResponseDTO> findAvailableVehicles(LocalDate date) {
-        return List.of();
+        log.debug("Finding available vehicles for date: {}", date);
+        return vehicleRepository.findAll().stream()
+                .map(this::toResponseDTO)
+                .toList();
     }
 }
-
